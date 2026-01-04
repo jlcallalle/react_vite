289be147faa7665bdaa92667b78401982adc6a6a
Jorge <vasslatam01@invex.com>
2026-01-04 17:20:00 -0500

    clear comments

--- my-react-app-global/src/pages/estado.jsx
+++ my-react-app-global/src/pages/estado.jsx
@@ -2,10 +2,7 @@ import Counter from "../components/Counter"
 import ToggleButton from "../components/ToggleButton"
 
 const App = () => {
-  /* const [count, setCount] = useState(0); */
-  // Estado inicial del contador
-  // count → valor actual del estado
-  // setCount → función para actualizar el estado
+
   return (
     <div>
       <Counter/>
