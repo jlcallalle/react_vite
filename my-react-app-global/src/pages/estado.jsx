28d983a415de3196d6456d5fe15deb2fc61650e9
Jorge <vasslatam01@invex.com>
2026-01-04 17:23:25 -0500

    setName use State formu input

--- my-react-app-global/src/pages/estado.jsx
+++ my-react-app-global/src/pages/estado.jsx
@@ -1,5 +1,6 @@
 import Counter from "../components/Counter"
 import ToggleButton from "../components/ToggleButton"
+import NameForm from "../components/NameForm";
 
 const App = () => {
 
@@ -7,6 +8,8 @@ const App = () => {
     <div>
       <Counter/>
       <ToggleButton />
+      <h2>Formulario de Nombre</h2>
+      <NameForm />
     </div>
   )
 }
